41f48d7174c29f688410c1c1ab8095781ce47835
Marius Butuc <marius.butuc@gmail.com>
2013-02-28 21:56:30 -0500

    Integrate Postgres community cookbook + dependencies
    http://community.opscode.com/cookbooks/postgresql/
    
     + openssl
     + build-essential

--- cookbooks/rails/recipes/default.rb
+++ cookbooks/rails/recipes/default.rb
@@ -1,35 +1,39 @@
 # 1. Install Nginx & Unicorn (from cookbooks)
 include_recipe "nginx"
 include_recipe "unicorn"
+include_recipe "postgresql::server"
 
 gem_package "bundler" do
   options("--no-ri --no-rdoc")
 end
 
+app_name = node[:app][:name]
+deploy_to = "/home/#{node[:user][:name]}/#{app_name}"
+
 # 3. Create necessary directories
-directory "/home/#{node[:user][:name]}/#{node[:app][:name]}" do
+directory "#{deploy_to}" do
   owner 'vagrant'
 end
 
-directory "/home/#{node[:user][:name]}/#{node[:app][:name]}" + '/current' do
+directory "#{deploy_to}" + '/current' do
   owner 'vagrant'
 end
 
 %w(config log tmp sockets pids).each do |dir|
-  directory "/home/#{node[:user][:name]}/#{node[:app][:name]}/shared/#{dir}" do
+  directory "#{deploy_to}/shared/#{dir}" do
     recursive true # mkdir -p
     mode 0755
   end
 end
 
 # 4. Configure the Unicorn app server
-template "#{node[:unicorn][:config_path]}/#{node[:app][:name]}.conf.rb" do
+template "#{node[:unicorn][:config_path]}/#{app_name}.conf.rb" do
   mode 0644
   source "unicorn.conf.erb"
 end
 
 # 5. Create Nginx config template for this app's Virtual Host
-nginx_config_path = "/etc/nginx/sites-available/#{node[:app][:name]}.conf"
+nginx_config_path = "/etc/nginx/sites-available/#{app_name}.conf"
 
 template nginx_config_path do
   mode 0644
@@ -37,7 +41,7 @@ template nginx_config_path do
   notifies :reload, "service[nginx]"
 end
 
-nginx_site "#{node[:app][:name]}" do
+nginx_site "#{app_name}" do
   config_path nginx_config_path
   action :enable
 end
